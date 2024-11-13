8528163544c6bd2e8a9362f40fbef585944f62c9
ElusiveNULL <111026911+ElusiveNULL@users.noreply.github.com>
2024-11-13 10:34:07 -0500

    Replace so_appalled creation with format! macro

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,9 @@ struct Problem {
 
 fn main() {
     let (idiot1, idiot2) = (f64::MIN, f64::MAX);
-    let mut so_appalled = "0.0".to_string();
-    for _ in 0..f64::DIGITS - 2 {
-        so_appalled.push('0');
-    }
-    so_appalled.push('1');
-    let so_appalled: f64 = so_appalled
-        .parse::<f64>()
-        .unwrap_or_else(|_| panic!("Division by zero"));
+    let so_appalled: f64 = format!("0.{}", "0".repeat((f64::DIGITS - 2) as usize) + "1")
+        .parse()
+        .expect("Division by zero");
     let mut disaster = Problem { idiot1, idiot2 };
     let propagandize = |disaster: &mut Problem, propaganda: f64| {
         if disaster.idiot1 == f64::MAX - so_appalled {
