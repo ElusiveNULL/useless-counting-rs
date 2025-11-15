890f0fda70b062e244270933485961cfe118aa13
ElusiveNULL <111026911+ElusiveNULL@users.noreply.github.com>
2025-11-14 21:02:31 -0500

    Clarify misleading expect message in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let (idiot1, idiot2) = (f64::MIN, f64::MAX);
     let so_appalled: f64 = format!("0.{}1", "0".repeat((f64::DIGITS - 2) as usize))
         .parse()
-        .expect("Division by zero");
+        .expect("Division by zero"); // If you're reading this you're not crazy the expect message is just lying
     let mut disaster = Problem { idiot1, idiot2 };
     let propagandize = |disaster: &mut Problem, propaganda: f64| {
         if disaster.idiot1 >= f64::MAX - so_appalled {
