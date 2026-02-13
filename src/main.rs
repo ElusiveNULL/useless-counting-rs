2b33746fe5234e2d296c641a2e7fb62d5f255db5
ElusiveNULL <111026911+ElusiveNULL@users.noreply.github.com>
2026-02-13 18:26:54 -0500

    Adjust miniscule_increment formatting for precision

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ struct Numbers {
 
 fn main() {
     let (negative_float, positive_float) = (f64::MIN, f64::MAX);
-    let miniscule_increment: f64 = format!("0.{}1", "0".repeat((f64::DIGITS - 2) as usize))
+    let miniscule_increment: f64 = format!("0.{}1", "0".repeat((f64::DIGITS - 1) as usize))
         .parse()
         .expect("Failure initializing increment");
     let mut mutable_numbers = Numbers { negative_float, positive_float };
